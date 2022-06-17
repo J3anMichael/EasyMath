8662e50a653f33bc854748b96a272543255bc038
Jean Michael <jeanmmclaudino@gmail.com>
2022-06-16 22:11:35 -0300

    Add project files.

---
+++ EasyMath/Model/Math.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyMath.Model
+{
+    public class Math
+    {
+        public static decimal Somar(decimal firstvalue, decimal secondvalue)
+        {
+            return firstvalue + secondvalue;
+        }
+
+        public static decimal Subtrair(decimal firstvalue, decimal secondvalue)
+        {
+            return firstvalue - secondvalue;
+        }
+
+        public static decimal Multiplicacao(decimal firstvalue, decimal secondvalue)
+        {
+            return firstvalue * secondvalue;
+        }
+
+        public static decimal Divisao(decimal firstvalue, decimal secondvalue)
+        {
+            return firstvalue / secondvalue;
+        }
+    }
+}
